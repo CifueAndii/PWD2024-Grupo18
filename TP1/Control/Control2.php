2b32abbac488f433800a501afd57ff12556a6c88
CifueAndii <117041628+CifueAndii@users.noreply.github.com>
2024-09-06 21:03:24 -0300

    Delete TP1 directory

--- TP1/Control/Control2.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class Control2{
-        public function suma($horas){
-            $suma = $horas['lunes'] + $horas['martes'] + $horas['miercoles'] + $horas['jueves'] + $horas['viernes'];
-            return $suma;
-        }
-    }
-?>

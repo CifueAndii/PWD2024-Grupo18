aee8dd53f0d2d1488f381d3fd5833120b43878bc
CifueAndii <117041628+CifueAndii@users.noreply.github.com>
2024-09-06 21:03:44 -0300

    Delete TP2 directory

--- TP2/Control/Control2_3.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class Control2_3 {
-        public function infoForm($datos){
-            $mensaje = "Hola, yo soy ".$datos["nombre"]." ".$datos["apellido"].", tengo ".$datos["edad"]." año/s y vivo en ".$datos["direccion"].".<br><br>";
-            return $mensaje;
-        }
-    }
-?>

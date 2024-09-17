44915fdf9c771f69a0e473c9eddd3adb2899644b
CifueAndii <117041628+CifueAndii@users.noreply.github.com>
2024-09-17 19:32:04 -0300

    Add files via upload

---
+++ Indice/Vista/Estructura/Indice.php
@@ -0,0 +1,103 @@
+<?php
+    $tituloPagina = "Indice";
+    include_once("Encabezado.php");
+?>
+
+<div id="index">
+    <section id="tp1">
+        <div>
+            <div class="text-center">
+                <h2 id="tp">Trabajo Práctico N°1</h2>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-10">
+                    <div class="list-group">
+                        <a href="../../../TP1/Vista/Ejercicio1.php" class="list-group-item list-group-item-action list-group-item-primary">Ejercicio 1</a>
+                        <a href="../../../TP1/Vista/Ejercicio2.php" class="list-group-item list-group-item-action list-group-item-secondary">Ejercicio 2</a>
+                        <a href="../../../TP1/Vista/Ejercicio3.php" class="list-group-item list-group-item-action list-group-item-success">Ejercicio 3</a>
+                        <a href="../../../TP1/Vista/Ejercicio4.php" class="list-group-item list-group-item-action list-group-item-danger">Ejercicio 4</a>
+                        <a href="../../../TP1/Vista/Ejercicio5.php" class="list-group-item list-group-item-action list-group-item-warning">Ejercicio 5</a>
+                        <a href="../../../TP1/Vista/Ejercicio6.php" class="list-group-item list-group-item-action list-group-item-info">Ejercicio 6</a>
+                        <a href="../../../TP1/Vista/Ejercicio7.php" class="list-group-item list-group-item-action list-group-item-light">Ejercicio 7</a>
+                        <a href="../../../TP1/Vista/Ejercicio8.php" class="list-group-item list-group-item-action list-group-item-dark">Ejercicio 8</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div>
+            <div class="text-center">
+                <h2 id="tp">Trabajo Práctico N°2</h2>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-10">
+                    <div class="list-group">
+                        <div class="list-group-item list-group-item-primary">Ejercicio 1</div>
+                        <blockquote class="blockquote text-center">
+                            <p class="mb-0" id="tp">Eligimos <b>JQuery</b> para realizar las validación de formularios.</p>
+                        </blockquote>
+                        <a href="../../../TP2/Vista/Ejercicio2_1.php" class="list-group-item list-group-item-action list-group-item-secondary">Ejercicio 2_1</a>
+                        <a href="../../../TP2/Vista/Ejercicio2_2.php" class="list-group-item list-group-item-action list-group-item-success">Ejercicio 2_2</a>
+                        <a href="../../../TP2/Vista/Ejercicio2_3.php" class="list-group-item list-group-item-action list-group-item-danger">Ejercicio 2_3</a>
+                        <a href="../../../TP2/Vista/Ejercicio2_4.php" class="list-group-item list-group-item-action list-group-item-warning">Ejercicio 2_4</a>
+                        <a href="../../../TP2/Vista/Ejercicio2_5.php" class="list-group-item list-group-item-action list-group-item-info">Ejercicio 2_5</a>
+                        <a href="../../../TP2/Vista/Ejercicio2_6.php" class="list-group-item list-group-item-action list-group-item-light">Ejercicio 2_6</a>
+                        <a href="../../../TP2/Vista/Ejercicio2_7.php" class="list-group-item list-group-item-action list-group-item-dark">Ejercicio 2_7</a>
+                        <a href="../../../TP2/Vista/Ejercicio2_8.php" class="list-group-item list-group-item-action list-group-item-primary">Ejercicio 2_8</a>
+                        <a href="../../../TP2/Vista/Ejercicio3.php" class="list-group-item list-group-item-action list-group-item-secondary">Ejercicio 3</a>
+                        <a href="../../../TP2/Vista/Ejercicio4.php" class="list-group-item list-group-item-action list-group-item-success">Ejercicio 4</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div>
+            <div class="text-center">
+                <h2 id="tp">Trabajo Práctico N°3</h2>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-10">
+                    <div class="list-group">
+                        <a href="../../../TP3/Vista/Ejercicio1.php" class="list-group-item list-group-item-action list-group-item-primary">Ejercicio 1</a>
+                        <a href="../../../TP3/Vista/Ejercicio2.php" class="list-group-item list-group-item-action list-group-item-secondary">Ejercicio 2</a>
+                        <a href="../../../TP3/Vista/Ejercicio3.php" class="list-group-item list-group-item-action list-group-item-success">Ejercicio 3</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div>
+            <div class="text-center">
+                <h2 id="tp">Trabajo Práctico N°4</h2>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-10">
+                    <div class="list-group">
+                        <div class="list-group-item list-group-item-primary">Ejercicio 1</div>
+                        <blockquote class="blockquote text-center">
+                            <p class="mb-0" id="tp">Creamos la capa del Modelo, generando los archivos Auto.php y Persona.php.</p>
+                        </blockquote>
+                        <div class="list-group-item list-group-item-primary">Ejercicio 2</div>
+                        <blockquote class="blockquote text-center">
+                            <p class="mb-0" id="tp">Creamos la capa de Control, generando los archivos AbmAuto.php y AbmPersona.php.</p>
+                        </blockquote>
+                        <a href="../../../TP4/Vista/Ejercicios/VerAutos.php" class="list-group-item list-group-item-action list-group-item-success">Ejercicio 3 (VerAutos.php)</a>
+                        <a href="../../../TP4/Vista/Ejercicios/BuscarAuto.php" class="list-group-item list-group-item-action list-group-item-danger">Ejercicio 4 (BuscarAuto.php)</a>
+                        <a href="../../../TP4/Vista/Ejercicios/ListaPersonas.php" class="list-group-item list-group-item-action list-group-item-warning">Ejercicio 5 (ListaPersonas.php)</a>
+                        <a href="../../../TP4/Vista/Ejercicios/NuevaPersona.php" class="list-group-item list-group-item-action list-group-item-info">Ejercicio 6 (NuevaPersona.php)</a>
+                        <a href="../../../TP4/Vista/Ejercicios/NuevoAuto.php" class="list-group-item list-group-item-action list-group-item-light">Ejercicio 7 (NuevoAuto.php)</a>
+                        <a href="../../../TP4/Vista/Ejercicios/CambioDuenio.php" class="list-group-item list-group-item-action list-group-item-dark">Ejercicio 8 (CambioDuenio.php)</a>
+                        <a href="../../../TP4/Vista/Ejercicios/BuscarPersona.php" class="list-group-item list-group-item-action list-group-item-primary">Ejercicio 9 (BuscarPersona.php) </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+<?php
+    include_once("Pie.php");
+?>
